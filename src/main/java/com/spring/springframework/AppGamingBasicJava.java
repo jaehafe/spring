db4d20933954c2ae270011e16c672ef7ba62eb1a
Jaeha Lee <wogkdkrm112@gmail.com>
2023-12-03 22:48:12 +0900

    init: spring project init

---
+++ src/main/java/com/spring/springframework/AppGamingBasicJava.java
@@ -0,0 +1,12 @@
+package com.spring.springframework;
+
+import com.spring.springframework.game.GameRunner;
+import com.spring.springframework.game.MarioGame;
+
+public class AppGamingBasicJava {
+    public static void main(String[] args) {
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner(marioGame);
+        gameRunner.run();
+    }
+}
